4fbaa990ba1518c8b5bc5f102515f61a4a70b153
Jose Antonio <josegrobles1997@gmail.com>
2018-11-28 11:28:13 +0100

    Stop on system close

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const mssql = require('./mssql');
 
 module.exports = () => {
+	let db;
 	const start = ({ logger, config }, cb) => {
 		mssql(config)
 			.start()
-			.then(db => cb(null, db))
+			.then(_db => {
+				db = _db;
+				cb(null, db);
+			})
 			.catch(err => {
 				logger.error(`${err.message} - ${err.stack}`);
 				cb(err);
 			});
 	};
 
-	return { start };
+	const stop = cb => {
+		db.pool && db.pool.close();
+		cb(null);
+	};
+
+	return { start, stop };
 };
